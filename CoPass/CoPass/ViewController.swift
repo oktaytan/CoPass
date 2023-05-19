0ba7b259aacca173fd8482defdfa459cda8183d8
Oktay Tanrıkulu <oktaytanrikulu@gmail.com>
2023-05-19 22:11:32 +0300

    Getting started

---
+++ CoPass/CoPass/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoPass
+//
+//  Created by Oktay Tanrıkulu on 19.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
